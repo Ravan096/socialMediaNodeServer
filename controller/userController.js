15376a6b98d1e5950fb3c59c17ba19561dc95805
Lakshman <guptalakshman96.lg@gmail.com>
2025-01-21 17:42:50 +0530

    fix: correct casing for production environment in cookie settings

--- controller/userController.js
+++ controller/userController.js
@@ -58,8 +58,8 @@ exports.userLogin = async (req, res, next) => {
         res.cookie("token", token, {
             expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
             httpOnly: true,
-            secure: process.env.NODE_ENV === "Production",
-            sameSite: 'None'
+            secure: process.env.NODE_ENV === "production",
+            sameSite: 'None',
         })
         res.status(200).json({
             success: true,
