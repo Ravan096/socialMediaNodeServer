fffecdafc20528d5ff9ce70a0d9bdedb72db3758
Lakshman <guptalakshman96.lg@gmail.com>
2025-01-21 17:29:00 +0530

    enhance CORS settings to allow specific origins and methods

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const cors = require("cors");
 
 
 const app = express();
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
+});
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
