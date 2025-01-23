79c5a047a4ac0ff3274ec01ac7eee3a37deb6cee
Lakshman <guptalakshman96.lg@gmail.com>
2025-01-23 18:09:02 +0530

    feat: integrate connect-mongo for session management and update database connection settings

--- config/database.js
+++ config/database.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDatabase = () => {
     mongoose.connect(process.env.DataUri, {
-        family: 4, useNewUrlParser: true,
-        useUnifiedTopology: true,
+        family: 4,
     }).then((data) => {
         console.log(`Mongodb is connected with server: http://${data.connection.host}:${data.connection.port}`);
     })
