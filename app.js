44397bfab0ba3dc77edf51b165df70be9fb44e2a
Lakshman <guptalakshman96.lg@gmail.com>
2025-02-12 15:46:37 +0530

    fix: update CORS policy to allow requests from the production domain

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Origin", "https://mitrajunction.netlify.app");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
