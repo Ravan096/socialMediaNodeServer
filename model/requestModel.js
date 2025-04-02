19f9b665ae9fb7136ee0b6975cca9098550e777b
Lakshman <guptalakshman96.lg@gmail.com>
2025-04-02 19:21:49 +0530

    feat: implement friend request functionality with sending and accepting requests; update user routes and models accordingly

--- model/requestModel.js
+++ model/requestModel.js
@@ -2,17 +2,25 @@ const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema({
     status: {
+        type: String,
+        default: "pending",
         enum: ["pending", "accepted", "rejected"]
     },
     sender: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Users"
+        ref: "Users",
+        required: true
     },
-    chat: {
+    // chat: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "Chat"
+    // },
+    receiver: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Chat"
-    },
+        ref: "Users",
+        required: true
+    }
 }, { timestamps: true })
 
 
-module.exports = mongoose.Model("RequestModel", requestSchema)
+module.exports = mongoose.model("RequestModel", requestSchema)
